79265577390c6ae5c0dfc5f3e87bd0ab066f1030
Thuvarakan <thuvamit2017@gmail.com>
2022-06-14 09:44:10 +0530

    correcttion

--- q4/Palindrome.java
+++ q4/Palindrome.java
@@ -1,22 +1,17 @@
-class SameChar{
+class Palindrome{
 	public static void main(String[] args){
-		String word1="Thuva",word2="Thivi";
-		if(word1.length()==word2.length()){
-		   char wordarr1[word1.length()];
-		   char wordarr2[word1.length()];
-	           for(int i=0;i<word1.length();i++){
-		     if(word1.charAt(i) != word2.charAt(i)){
-		     	System.out.println("word 1 and word 2 are not same");
-       			break;
-		     }
-		     else{
-		     	wordarr1[i]=word1.charAt(i);
-			wordarr2[i]=word2.charAt(i);
-		    }
-		   }
+		String word="levewl";
+		String reverse="";
+		int length=word.length();
+		for(int i=(length-1);i>=0;i--)
+		{
+			reverse=reverse + word.charAt(i);
+		}
+		if(word.equals(reverse)){
+			System.out.println("Given Word is Palindrome");
 		}
 		else{
-		  System.out.println("word 1 and word 2 are not same");
+			System.out.println("Given Word is not a Palindrome");
 		}
 	}	
 }
